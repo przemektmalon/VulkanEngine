7e9ca2223cd8b70469ecc846c282de63c96b3956
Przemek Malon <przemekmalon@hotmail.com>
2018-03-05 16:25:07 +0000

    Fix File.cpp

---
+++ src/File.cpp
@@ -0,0 +1,122 @@
+#include "PCH.hpp"
+#include "File.hpp"
+
+bool File::create(std::string && pPath, Mode pFileMode)
+{
+	if (pPath.length() > 127)
+		return false;
+	meta.path = pPath;
+	meta.fileMode = pFileMode;
+
+	return create(meta.fileMode);
+}
+
+bool File::create(Mode pFileMode)
+{
+	if (meta.path.length() == 0)
+		return false;
+
+	meta.fileMode = pFileMode;
+
+	file.open(meta.path.c_str(), (int)meta.fileMode);
+}
+
+bool File::open(std::string && pPath, Mode pFileMode)
+{
+	return open(pPath, pFileMode);
+}
+
+bool File::open(std::string & pPath, Mode pFileMode)
+{
+	if (pPath.length() > 127)
+		return false;
+	meta.path = pPath;
+	meta.fileMode = pFileMode;
+
+	return open(meta.fileMode);
+}
+
+bool File::open(Mode pFileMode)
+{
+	if (meta.path.length() == 0)
+		return false;
+
+	meta.fileMode = pFileMode;
+
+	file.open(meta.path.c_str(), (int)meta.fileMode);
+	if (file.good() && file.is_open() && !file.bad())
+	{
+		file.seekg(0, std::ios_base::end);
+		meta.size = (size_t)file.tellg();
+		file.seekg(0);
+		return true;
+	}
+	else
+		return false;
+}
+
+void File::write(void * data, u32 size)
+{
+	file.write((char*)data, size);
+}
+
+void File::writeStr(std::string & string, bool includeTerminator, bool writeCapacity)
+{
+	if (!writeCapacity)
+		file.write(string.c_str(), string.length() + (includeTerminator ? 1 : 0));
+	else
+		file.write(string.c_str(), string.size() + (includeTerminator ? 1 : 0));
+}
+
+void File::writeStr(std::string & string, bool includeTerminator)
+{
+	file.write(string.c_str(), string.length() + (includeTerminator ? 1 : 0));
+}
+
+void File::read(void * data, u32 size)
+{
+	file.read((char*)data, size);
+}
+
+void File::read(std::string & string, u32 length)
+{
+	string.resize(length);
+	file.read((char*)string.c_str(), length);
+}
+
+void File::readStr(std::string & string, char delim, int size)
+{
+	if (size == -1)
+	{
+		char lastChar[2];
+		lastChar[1] = 0;
+		file.read(lastChar, 1);
+		while (*lastChar != delim && *lastChar != '\0' && !file.eof())
+		{
+			string.append(lastChar);
+			file.read(lastChar, 1);
+		}
+	}
+	else
+	{
+		string.resize(size);
+		file.read(const_cast<char*>(string.c_str()), size);
+	}
+}
+
+void File::readFile(void * buffer)
+{
+	readArray((char*)buffer, meta.size);
+}
+
+char File::peekChar()
+{
+	return file.peek();
+}
+
+char File::pullChar()
+{
+	char pull;
+	file.read(&pull, 1);
+	return pull;
+}
