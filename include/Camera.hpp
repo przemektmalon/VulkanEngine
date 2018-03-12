4c209df114c344f9bf47280615da140604995205
Przemek Malon <przemekmalon@hotmail.com>
2018-03-12 15:45:56 +0000

    Camera class

---
+++ include/Camera.hpp
@@ -0,0 +1,64 @@
+#pragma once
+#include "PCH.hpp"
+#include "Time.hpp"
+
+class Camera
+{
+public:
+	Camera();
+	~Camera() {}
+
+	void initialiseProj(float pAspect, float pFOV = glm::pi<float>() / 2.5f, float pNear = 0.1f, float pFar = 1000000.f);
+
+	void recalculateProj();
+
+	void initaliseOrtho(int sizeX, int sizeY, float pNear = 0.99f, float pFar = 100.f);
+
+	void update(Time& dt);
+
+	void setFOV(float pFOV);
+
+	void move(glm::fvec3 move) { targetPos += move; }
+	void rotate(float roll, float pitch, float yaw) { targetRoll += roll; targetPitch += pitch; targetYaw += yaw; }
+
+	glm::fvec3 getDirectionVector();
+	glm::fvec3 getPosition() { return pos; }
+	glm::quat getQuaternion() { return qRot; }
+	glm::fmat4 getProj() { return proj; }
+	glm::fmat4 getView() { return view; }
+	glm::fmat4 getInverseProj() { return inverseProj; }
+	glm::fmat4 getInverseView() { return inverseView; }
+
+	glm::fmat4 getMatYaw() { return matYaw; }
+	glm::fmat4 getMatPitch() { return matPitch; }
+	glm::fmat4 getMatRoll() { return matRoll; }
+
+private:
+
+	float ler(float a, float b, float N)
+	{
+		float v = ((a * (N - 1)) + b) / N;
+		return v;
+	}
+
+	glm::fvec3 pos;
+	glm::fvec3 targetPos;
+	float yaw, pitch, roll;
+	float targetYaw, targetPitch, targetRoll;
+	glm::fmat4 rotation;
+	glm::quat qRot;
+
+	glm::fmat4 proj, view, projView;
+	glm::fmat4 inverseProj;
+	glm::fmat4 inverseView;
+
+	glm::fmat4 matRoll, matPitch, matYaw;
+
+	float nearClip, farClip;
+	float fov, aspect;
+
+	float turnLerpFactor;
+	float moveLerpFactor;
+
+	glm::fvec4 viewRays[4];
+};
---
+++ src/Camera.cpp
@@ -0,0 +1,85 @@
+#include "Camera.hpp"
+#include "Engine.hpp"
+#include "Renderer.hpp"
+
+Camera::Camera()
+{
+	yaw = 0; pitch = 0; roll = 0;
+	targetYaw = 0; targetPitch = 0; targetRoll = 0;
+	pos = glm::fvec3(0, 0, 0);
+	targetPos = glm::fvec3(0, 0, 0);
+	turnLerpFactor = 1.15;
+	moveLerpFactor = 3.f;
+}
+
+void Camera::initialiseProj(float pAspect, float pFOV, float pNear, float pFar)
+{
+	nearClip = pNear;
+	farClip = pFar;
+	fov = pFOV;
+	aspect = pAspect;
+
+	recalculateProj();
+}
+
+void Camera::recalculateProj()
+{
+	proj = glm::perspective(fov, aspect, nearClip, farClip);
+	inverseProj = glm::inverse(proj);
+}
+
+void Camera::initaliseOrtho(int sizeX, int sizeY, float pNear, float pFar)
+{
+	proj = glm::ortho(0.f, float(sizeX), 0.f, float(sizeY), pNear, pFar);
+}
+
+glm::fvec3 Camera::getDirectionVector()
+{
+	return glm::fvec3(glm::sin(yaw) * glm::cos(pitch), -glm::sin(pitch), -glm::cos(yaw) * glm::cos(pitch));
+}
+
+void Camera::update(Time & dt)
+{
+	targetPitch = glm::clamp(targetPitch, -glm::pi<float>() / 2.f, glm::pi<float>() / 2.f);
+	pitch = ler(pitch, targetPitch, turnLerpFactor);
+	yaw = ler(yaw, targetYaw, turnLerpFactor);
+
+	matRoll = glm::rotate(glm::fmat4(), roll, glm::vec3(0.0f, 0.0f, 1.0f));
+	matPitch = glm::rotate(glm::fmat4(), pitch, glm::vec3(1.0f, 0.0f, 0.0f));
+	matYaw = glm::rotate(glm::fmat4(), yaw, glm::vec3(0.0f, 1.0f, 0.0f));
+
+	pos.x = ler(pos.x, targetPos.x, moveLerpFactor);
+	pos.y = ler(pos.y, targetPos.y, moveLerpFactor);
+	pos.z = ler(pos.z, targetPos.z, moveLerpFactor);
+
+	glm::fmat4 translate = glm::fmat4(1.0f);
+	translate = glm::translate(translate, -pos);
+
+	qRot = glm::angleAxis(targetPitch, glm::vec3(1.0f, 0.0f, 0.0f));
+	qRot *= glm::angleAxis(targetYaw, glm::vec3(0.0f, 1.0f, 0.0f));
+
+	rotation = glm::fmat4(qRot);
+	view = rotation * translate;
+	inverseView = glm::inverse(view);
+
+	projView = proj * view;
+
+	viewRays[0] = glm::fvec4(-1.f, -1.f, -1.f, 1.f);//BL
+	viewRays[1] = glm::fvec4(1.f, -1.f, -1.f, 1.f);//BR
+	viewRays[2] = glm::fvec4(1.f, 1.f, -1.f, 1.f);//TR
+	viewRays[3] = glm::fvec4(-1.f, 1.f, -1.f, 1.f);//TL
+
+	for (int i = 0; i < 4; ++i)
+	{
+		viewRays[i] = inverseProj * viewRays[i];
+		viewRays[i] /= viewRays[i].w;
+		viewRays[i] /= viewRays[i].z;
+	}
+}
+
+void Camera::setFOV(float pFOV)
+{
+	fov = glm::radians(pFOV);
+	recalculateProj();
+	//Engine::renderer->cameraProjUpdated();
+}
