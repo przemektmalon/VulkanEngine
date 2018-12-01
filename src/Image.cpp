21b7cbb1c9f5cd12fd40e7ac682d8ddfbac2ef5b
Przemek Malon <przemekmalon@hotmail.com>
2018-12-01 18:32:07 +0000

    Remove unnecessary dependancy

--- src/Image.cpp
+++ src/Image.cpp
@@ -4,7 +4,6 @@
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
 #include "stb_image_write.h"
-#include "cro_mipmap.h"
 #include "Engine.hpp"
 #include "Console.hpp"
 
@@ -48,9 +47,3 @@ void Image::save(std::string path)
 	int result = stbi_write_png(path.c_str(), width, height, components, &data[0], 0);
 }
 
-void Image::generateMipMap(Image & mipmapped)
-{
-	mipmapped.data.clear();
-	mipmapped.setSize(width >> 1, height >> 1);
-	cro_GenMipMapAvgI((int*)&data[0], width, height, (int*)&mipmapped.data[0]);
-}
