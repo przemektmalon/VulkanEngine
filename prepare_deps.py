4ebbce6bec7c4c80144b07f6acf21affea8e6a0f
Przemek Malon <przemekmalon@hotmail.co.uk>
2019-02-05 04:37:26 +0000

    Added script to automatically clone and configure most of the dependencies into lib folder, these are now compiled as part of the project. Set c++ standard to c++17. Replaced GLOB-bing source files with add_subdirectory and manual additions. Added lib folder to gitignore. Now using find_package(Vulkan) instead of having to manually set the locations. Added CMake macro to setup MSVC project precompiled headers, also set MSVC /bigobj flag.

---
+++ prepare_deps.py
@@ -0,0 +1,38 @@
+
+# Clones dependecies from github and checks out working branches
+
+import os
+from subprocess import call
+import urllib.request
+import zipfile
+
+if __name__ == "__main__":
+
+    git_vdu = "https://github.com/przemektmalon/VulkanDevUtility.git"
+    git_freetype = "https://github.com/winlibs/freetype.git"
+    git_chaiscript = "https://github.com/ChaiScript/ChaiScript.git"
+    git_glm = "https://github.com/g-truc/glm.git"
+    git_rapidxml = "https://github.com/dwd/rapidxml.git"
+    url_stb_image = "https://raw.githubusercontent.com/nothings/stb/master/stb_image.h"
+    url_stb_image_write = "https://raw.githubusercontent.com/nothings/stb/master/stb_image_write.h"
+
+    call("git clone " + git_vdu + " ./lib/vdu --quiet", shell=True)
+    call("git clone " + git_freetype + " ./lib/freetype --quiet", shell=True)
+    call("git clone " + git_chaiscript + " ./lib/chaiscript --quiet", shell=True)
+    call("git clone " + git_glm + " ./lib/glm --quiet", shell=True)
+    call("git clone " + git_rapidxml + " ./lib/rapidxml --quiet", shell=True)
+
+    os.chdir("lib/chaiscript")
+    call("git checkout release-6.x", shell=True)
+
+    os.chdir("../glm")
+    # The newest versions of glm don't seem to work (models don't draw)
+    # TODO: find out why ?
+    call("git checkout 8f39bb8")
+    os.chdir("..")
+
+    os.makedirs("lib/stb/stb")
+
+    urllib.request.urlretrieve(url_stb_image, "lib/stb/stb/stb_image.h")
+    urllib.request.urlretrieve(url_stb_image_write, "lib/stb/stb/stb_image_write.h")
+
