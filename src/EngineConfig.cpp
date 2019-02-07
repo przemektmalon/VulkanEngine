749139deb987eec46926756d8375959ff1f0ee4b
Przemek Malon <przemekmalon@hotmail.co.uk>
2019-02-07 01:21:10 +0000

    Move misplaced cpp file to src directory

--- src/EngineConfig.cpp
+++ src/EngineConfig.cpp
@@ -1,1 +1,25 @@
 #include "PCH.hpp"
+#include "EngineConfig.hpp"
+#include "Engine.hpp"
+#include "Window.hpp"
+#include "Threading.hpp"
+
+void postMessage(std::string msg, glm::fvec3 col)
+{
+	Engine::console->postMessage(msg, col);
+}
+
+void EngineConfig::Render::setResolution(glm::ivec2 set)
+{
+	if (set.x <= 0 || set.y <= 0) {
+		postMessage("Invalid Render resolution setting. Must be greater than 0", ERROR_COL);
+		return;
+	}
+	resolution = set;
+	auto resizeJobFunc = std::bind([](glm::ivec2 res) -> void {
+		Engine::window->resize(res.x, res.y);
+		Engine::physicalDevice->querySurfaceCapabilities(Engine::window->vkSurface);
+		Engine::config.changedSpecials.insert(Render_Resolution);
+	},set);
+	Engine::threading->addGPUJob(new Job<decltype(resizeJobFunc)>(resizeJobFunc, JobBase::GPU));
+}
