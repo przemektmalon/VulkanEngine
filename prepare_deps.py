2055ed7fa57a18db7f51793839cf4a315a5189ba
Przemek Malon <przemekmalon@hotmail.co.uk>
2019-02-07 01:19:28 +0000

    Automatically download Assimp dependacy through prepare_deps.py and link in CMake. Configure Assimp CMake variables to only include necessary components and force building static lib

--- prepare_deps.py
+++ prepare_deps.py
@@ -13,6 +13,7 @@ if __name__ == "__main__":
     git_chaiscript = "https://github.com/ChaiScript/ChaiScript.git"
     git_glm = "https://github.com/g-truc/glm.git"
     git_rapidxml = "https://github.com/dwd/rapidxml.git"
+    git_assimp = "https://github.com/assimp/assimp.git"
     url_stb_image = "https://raw.githubusercontent.com/nothings/stb/master/stb_image.h"
     url_stb_image_write = "https://raw.githubusercontent.com/nothings/stb/master/stb_image_write.h"
 
@@ -21,6 +22,7 @@ if __name__ == "__main__":
     call("git clone " + git_chaiscript + " ./lib/chaiscript --quiet", shell=True)
     call("git clone " + git_glm + " ./lib/glm --quiet", shell=True)
     call("git clone " + git_rapidxml + " ./lib/rapidxml --quiet", shell=True)
+    call("git clone " + git_assimp + " ./lib/assimp --quiet", shell=True)
 
     os.makedirs("lib/stb/stb")
     urllib.request.urlretrieve(url_stb_image, "lib/stb/stb/stb_image.h")
@@ -30,6 +32,7 @@ if __name__ == "__main__":
     call("git checkout release-6.x", shell=True)
 
     os.chdir("../glm")
+
     # The newest versions of glm don't seem to work (models don't draw)
     # TODO: find out why ?
     call("git checkout 8f39bb8", shell=True)
